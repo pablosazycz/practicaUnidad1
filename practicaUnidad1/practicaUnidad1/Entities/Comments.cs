d1bfe314367e256cf5e2620d25693ea3ca831d78
pablosazycz <87151305+pablosazycz@users.noreply.github.com>
2022-01-17 23:59:33 -0300

    Add files via upload

---
+++ practicaUnidad1/practicaUnidad1/Entities/Comments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace practicaUnidad1.Entities
+{
+    internal class Comments
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+
+        public User User { get; set; }
+
+        
+    }
+}
---
+++ practicaUnidad1/practicaUnidad1/Entities/Posts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace practicaUnidad1.Entities
+{
+    internal class Posts
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
